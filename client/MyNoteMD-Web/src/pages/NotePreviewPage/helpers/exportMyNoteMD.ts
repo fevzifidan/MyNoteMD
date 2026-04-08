4aca804052e4e6611436541cdfac91832b906578
Fevzi FİDAN <fevzifidan05@gmail.com>
2026-04-08 19:06:20 +0300

    Memory leak issue in .mynotemd export type has been prevented, character set has been corrected.

--- client/MyNoteMD-Web/src/pages/NotePreviewPage/helpers/exportMyNoteMD.ts
+++ client/MyNoteMD-Web/src/pages/NotePreviewPage/helpers/exportMyNoteMD.ts
@@ -1,9 +1,10 @@
 export default function downloadMyNoteMD(content: string, id: string) {
     const element = document.createElement("a");
-    const blob = new Blob([content || ""], { type: "text/plain" });
+    const blob = new Blob([content || ""], { type: "text/plain;charset=utf-8" });
     element.href = URL.createObjectURL(blob);
     element.download = `${id}.mynotemd`;
     document.body.appendChild(element);
     element.click();
     document.body.removeChild(element);
+    window.URL.revokeObjectURL(element.href);
 };
